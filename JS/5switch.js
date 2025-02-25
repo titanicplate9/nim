1cd75a2f9fd5ec362ac03a2883efca87f34b3cb3
Nima <gnima256@gmail.com>
2025-02-25 19:30:44 +0330

    Add JavaScript and HTML files for day of the week display

---
+++ JS/5switch.js
@@ -0,0 +1,28 @@
+var today = new Date().getday();
+var day;
+
+switch (today) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case 6:
+    day = "Saturday";
+    break;
+}
+document.write("Today is: " + day);
+console.log("Today is: " + day);
