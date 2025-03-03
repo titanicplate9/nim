44e689bcc6c5cf7144b8cacb291e4075fcd3993d
Nima <gnima256@gmail.com>
2025-03-03 16:46:03 +0330

    dvdv

---
+++ JS/11forloop.js
@@ -0,0 +1,11 @@
+const cars = ["benz", "bmw", "peykan", "pride", "alfa"];
+
+// console.log("the car brand is : " + cars[0]);
+// console.log("the car brand is : " + cars[1]);
+// console.log("the car brand is : " + cars[2]);
+// console.log("the car brand is : " + cars[3]);
+// console.log("the car brand is : " + cars[4]);
+
+for (let i = 0; i <= 4; i++) {
+  document.write("the car brand is : " + cars[i] + "<br>");
+}
